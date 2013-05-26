91e04f6b9b3f399837b147f0a4a3fcc1ddd98832
Ryo Miyake <ryo.studiom@gmail.com>
2013-05-27 01:06:15 +0900

    Version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='tcptest',
     packages=['tcptest'],
-    version='0.2.2',
+    version='0.2.3',
     author='Ryo Miyake',
     author_email='ryo.studiom@gmail.com',
     description="Testing TCP program, following the Perl's Test::TCP "
